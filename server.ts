9bf1132dcafd257213dffc0c5bf1b5bc6a0cb749
KyoungWan <kyngwan@gmail.com>
2024-02-15 01:38:59 +0900

    Load msm data

--- server.ts
+++ server.ts
@@ -1,6 +1,12 @@
 import * as grpc from "@grpc/grpc-js";
 import * as protoLoader from "@grpc/proto-loader";
 import path from "path";
+import fs from "fs";
+import {
+  Point,
+  bigintPair,
+  MultiScalarMultiplication,
+} from "multi-scalar-multiplication";
 // Structure to hold client information and stream
 interface Client {
   id: string;
@@ -16,6 +22,38 @@ interface StreamRequest {
 interface ComputationData {
   data: string;
 }
+
+// load msm data
+const scalarData = fs.readFileSync(
+  path.join(__dirname, "data", "scalars.txt"),
+  "utf8"
+);
+const rawScalars = scalarData.split("\n");
+const scalarDataArray = rawScalars.map((s) => BigInt(s));
+
+const baseData = fs.readFileSync(
+  path.join(__dirname, "data", "bases.txt"),
+  "utf8"
+);
+const rawBases = baseData.split("\n");
+const baseDataArray = rawBases.map((b) => {
+  const [left, right] = b.split(", ");
+  return {
+    x: BigInt(left.replace("(", "")),
+    y: BigInt(right.replace(")", "")),
+  };
+});
+
+// // Define Curve
+// const a: bigint = 0n;
+// const b: bigint = 3n;
+// const p: bigint =
+//   21888242871839275222246405745257275088696311157297823662689037894645226208583n;
+
+// const msm = new MultiScalarMultiplication(a, b, p);
+// msm.loadData(scalarDataArray, baseDataArray);
+
+// const result: Point = msm.calculate();
 
 // Define the path to your .proto file
 const PROTO_PATH = path.resolve(__dirname, "computation.proto");
@@ -68,8 +106,15 @@ const streamComputationData: grpc.handleServerStreamingCall<
   // Check if 4 clients have connected, then broadcast a message
   if (clients.length === 4) {
     console.log("Broadcasting message to all clients");
-    clients.forEach((client) => {
-      client.stream.write({ data: "hello" }); // Replace 'hello' with your actual data
+    clients.forEach((client, index) => {
+      const base = baseDataArray.slice(index * 256, index * 256 + 255);
+      const scalar = scalarDataArray.slice(index * 256, index * 256 + 255);
+      client.stream.write({
+        data: {
+          base,
+          scalar,
+        },
+      }); // Replace 'hello' with your actual data
     });
   } else {
     console.log(`Waiting for more clients. Current count: ${clients.length}`);
